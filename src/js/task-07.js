05c2e704d760c8539a0ce5ba752d13ed59d7a1ca
FlBorys <131404105+FlBorys@users.noreply.github.com>
2023-08-30 15:17:30 +0300

    Delete src directory

--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const inputSize = document.getElementById('font-size-control');
-const frontSizeSpan = document.getElementById('text');
-
-inputSize.addEventListener('input', onInputSize);
-function onInputSize(event) {
-    frontSizeSpan.style.fontSize = `${event.currentTarget.value}px`;
-};
